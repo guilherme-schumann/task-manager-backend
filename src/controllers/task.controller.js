02a86ba3d66be7ecac8c023c40a689697b91157c
Guilherme Schumann <gui.rsc45@gmail.com>
2025-04-25 20:21:29 -0300

    feat: move create task endpoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -6,7 +6,7 @@ class TaskController {
         this.res = res;
     }
 
-    async getTasks() {
+    async getAll() {
         try {
             const tasks = await TaskModel.find({});
             this.res.status(200).send(tasks);
@@ -15,7 +15,7 @@ class TaskController {
         }
     }
 
-    async getTaskById() {
+    async getById() {
         try {
             const taskId = this.req.params.id;
             const task = await TaskModel.findById(taskId);
@@ -26,6 +26,16 @@ class TaskController {
                     .send("Essa tarefa não foi encontrada.");
             }
             return this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async create() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+            await newTask.save();
+            this.res.status(201).send(newTask);
         } catch (error) {
             this.res.status(500).send(error.message);
         }
