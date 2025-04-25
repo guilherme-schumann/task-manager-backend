30700478201de59bb3acde1669a09cc3cf9250d6
Guilherme Schumann <gui.rsc45@gmail.com>
2025-04-25 20:31:17 -0300

    feat: move delete task endpoint to a controller

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -22,21 +22,7 @@ router.patch("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const tasskId = req.params.id;
-
-        const taskToDelete = await TaskModel.findById(taskId);
-
-        if (!taskToDelete) {
-            return res.status(404).send("Essa tarefa não foi encontrada.");
-        }
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).delete();
 });
 
 module.exports = router;
